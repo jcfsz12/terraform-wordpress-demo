f32c13738b4bac798b62d7f63e924ca93f0959e8
Juan <105559028+jcfsz12@users.noreply.github.com>
2024-09-08 17:29:51 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,3 +67,52 @@ resource "aws_route_table_association" "public_subnet_2"{
     subnet_id = "${aws_subnet.second.id}"
     route_table_id = "${aws_route_table.crt.id}"
 }
+
+# add rds database as module
+module "database" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = ">= 5.6.0, < 6.0.0"
+
+  identifier            = "codio-database"
+  engine                = "mysql"
+  allocated_storage     = 5
+  instance_class        = "db.t3.micro"
+  db_name               = local.database
+  username              = local.user
+  port                  = "3306"
+  major_engine_version  = "8.0"
+  family                = "mysql8.0"
+  
+  create_db_subnet_group = true
+  subnet_ids             = [aws_subnet.main.id, aws_subnet.second.id]
+  vpc_security_group_ids = [aws_security_group.database.id]
+
+  apply_immediately   = true
+  skip_final_snapshot = true
+  deletion_protection = false
+  tags = local.tags
+}
+
+
+# create security group for database access
+resource "aws_security_group" "database" {
+  vpc_id = aws_vpc.main.id
+  name = "allow database connection"
+  ingress {
+    description      = "Mysql from VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = [aws_subnet.main.cidr_block, aws_subnet.second.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = local.tags
+}
